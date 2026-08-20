8f050faf8264a9c7c8b63e84fb984818b670e707
reynaldo480 <reynaldoilustrisimo96@gmail.com>
2026-08-20 11:38:43 +0800

    Complete

---
+++ home/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from .models import Student
+
+def home(request):
+    students = Student.objects.all()
+    return render(request, 'home/index.html', {'students': students})
+
+# Create your views here.
