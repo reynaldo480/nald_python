94137759f70aa9fd09973993b42142c3798d7b8f
reynaldo480 <reynaldoilustrisimo96@gmail.com>
2026-08-21 18:35:52 +0800

    Update

--- store/models.py
+++ store/models.py
@@ -4,9 +4,7 @@ from django.db import models
 class Product(models.Model):
     name = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    quantity = models.IntegerField(default=0)
+    stock = models.IntegerField(default=0)
 
     def __str__(self):
         return self.name
-
-# Create your models here.
